61f0fb399f152af0083db593f4ac6238fa0e83ab
Muhammad Shahid <shahid.jamal0343@gmail.com>
2017-04-12 14:34:13 +0500

    new cat file included

---
+++ _categ.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Categorie</title>
+        <link type="text/css" rel="stylesheet" href="css/bootstrap.min.css">
+        <link type="text/css" rel="stylesheet" href="css/categ.css">
+        <script src="js/jquery-3.1.1.min.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+    </head>
+    <body>
+        <div class="page-loading-icon"></div>
+        
+    </body>
+</html>
